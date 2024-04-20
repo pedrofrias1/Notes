6281db37f63e7c3f0d0a70dd12926132f8d6a8dc
pedro frias <pepof610@gmail.com>
2024-04-20 20:29:05 -0300

    found carpeta

--- src/components/carp.jsx
+++ src/components/carp.jsx
@@ -13,12 +13,15 @@ function Carp(){
         <Fragment>
             <div className="cart flex flex-col justify-center items-start mt-5">
                 <h3 className="font-bold text-2xl pb-3">Carpeta</h3>
-                <form onSubmit={(event)=>cargargarCarpeta(event)}></form>
-                <textarea className="item text-2xl font-semibold" placeholder="Ingrese su nombre de carpeta" autoComplete="off" name="titulo" id="titulo" cols="35" rows="5" required></textarea>
+                <form action="http://localhost:5000/cargarCarp" onSubmit={(event)=>cargargarCarpeta(event)} method="POST">
+                   <textarea className="item text-2xl font-semibold" placeholder="Ingrese su nombre de carpeta" autoComplete="off" name="titulo" id="titulo" cols="35" rows="5" required></textarea>
                 <div className="flex justify-center items-end gap-2 pt-3">
                    <i class="bi bi-x-square-fill text-2xl"></i>
                    <i class="bi bi-floppy text-2xl"></i> 
-                </div>   
+                   <input type="submit" value="enviar" />
+                </div>    
+                </form>
+                
             </div>
             
         </Fragment>
